16dfbbb5ad268814540f23f2939cb5d1270912e8
wusaYa <wusaYa@outlook>
2018-01-26 14:09:32 +0800

    once creat

---
+++ WSMutableTipsView/WSMutableTipsView/WSTipsManger.h
@@ -0,0 +1,13 @@
+//
+//  WSTipsManger.h
+//  WSMutableTipsView
+//
+//  Created by 吴洒 on 2018/1/26.
+//  Copyright © 2018年 吴洒. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WSTipsManger : NSObject
+
+@end
---
+++ WSMutableTipsView/WSMutableTipsView/WSTipsManger.m
@@ -0,0 +1,13 @@
+//
+//  WSTipsManger.m
+//  WSMutableTipsView
+//
+//  Created by 吴洒 on 2018/1/26.
+//  Copyright © 2018年 吴洒. All rights reserved.
+//
+
+#import "WSTipsManger.h"
+
+@implementation WSTipsManger
+
+@end
